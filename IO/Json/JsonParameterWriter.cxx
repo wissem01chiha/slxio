d3b9c4d24e649ad199bf6353058365e6fe7035d5
wissem01chiha <chihawissem08@gmail.com>
2025-12-27 16:08:21 +0100

    case sensitive naming windows patch fixing

--- IO/Json/JsonParameterWriter.cxx
+++
@@ -1,1 +0,0 @@
-#include "JsonParameterWriter.h"
--- IO/Slx/SlxPortParser.cxx
+++
@@ -1,9 +0,0 @@
-#include "SLXPortParser.h"
-
-SimulinkErrorType SimulinkPortParser::build(xmlNodePtr nodePtr) {
-  return SimulinkErrorType::SLX_OK;
-}
-
-std::shared_ptr<SimulinkPort> SimulinkPortParser::get() {
-  return std::shared_ptr<SimulinkPort>(std::move(p_));
-}
