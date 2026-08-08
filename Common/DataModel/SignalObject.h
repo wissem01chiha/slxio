aa8531494532f5a876a9b811c3c77729b2cc2342
wissem chiha <chihawissem08@gmail.com>
2026-08-08 20:54:07 +0100

    clang-format

--- Common/DataModel/SignalObject.h
+++ Common/DataModel/SignalObject.h
@@ -12,23 +12,23 @@ namespace slxio {
 SLXIO_ABI_NAMESPACE_BEGIN
 
 /**
- * @class SignalObject 
- * @brief 
+ * @class SignalObject
+ * @brief
  */
-template<typename T>
-class SLXIO_APIEXPORT SignalObject  {
+template <typename T> class SLXIO_APIEXPORT SignalObject {
 public:
-    SignalObject();
-    explicit SignalObject(size_t size);
-    ~SignalObject() = default;
+  SignalObject();
+  explicit SignalObject(size_t size);
+  ~SignalObject() = default;
 
-    virtual void SetData(const std::vector<T>& data) = 0 ;
-    virtual  const std::vector<T>& GetData() const = 0;
-    virtual  std::vector<T>& GetData() = 0;
+  virtual void SetData(const std::vector<T> &data) = 0;
+  virtual const std::vector<T> &GetData() const = 0;
+  virtual std::vector<T> &GetData() = 0;
 
-    virtual  size_t Size() const = 0 ;
+  virtual size_t Size() const = 0;
+
 protected:
-    std::vector<T> m_buffer;
+  std::vector<T> m_buffer;
 };
 
 SLXIO_ABI_NAMESPACE_END
